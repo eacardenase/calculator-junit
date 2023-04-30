5f6af3744f2482789eb3cadfc6592e1828693490
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-29 20:39:10 -0500

    feat: refactor and more test cases

--- src/test/com/eacardenase/calculator/CalculatorTest.java
+++ src/test/com/eacardenase/calculator/CalculatorTest.java
@@ -1,17 +1,44 @@
 package com.eacardenase.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void canAddZeroPlusZero() {
-        Calculator calculator = new Calculator();
-
         int result = calculator.add(0, 0);
 
         assertEquals(0, result, "Was expecting sum of 0");
+    }
+
+    @Test
+    public void canAddOnePlusOne() {
+        int result = calculator.add(1, 1);
+
+        assertEquals(2, result, "Was expecting sum of 0");
+    }
+
+    @Test
+    public void canAddMinusOnePlusOne() {
+        int result = calculator.add(-1, 1);
+
+        assertEquals(0, result, "Was expecting sum of 0");
+    }
+
+    @Test
+    public void canAddMinusOnePlusMinusOne() {
+        int result = calculator.add(-1, -1);
+
+        assertEquals(-2, result, "Was expecting sum of 0");
     }
 }
